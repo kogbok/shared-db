fb203073a74aaca71f64127cee6bc058490fc92a
kogbok <kogbok@gmail.com>
2017-03-28 08:47:29 +0200

    cleaning

--- modules/core/src/medium/logger_paxos.ts
+++ modules/core/src/medium/logger_paxos.ts
@@ -3,14 +3,31 @@ import {ActorId} from '../types'
 export module logger_paxos {
 
 export interface ILogger {
-  write();
-  read();
+  put();
+  read_since();
 }
 
 export class Acceptor {
   private actor_id_: ActorId;
   private logger_: ILogger;
+
+  send(msg: AcceptorMessage) {
+    this.logger_.put();
+  }
 }
+
+export type AcceptorMessage = {};
+
+export class AcceptorSet {
+  private acceptors_: Array<Acceptor>;
+
+  send(msg: AcceptorMessage) {
+    for (let a of this.acceptors_) {
+      a.send(msg);
+    }
+  }
+}
+
 
 export class Proposer {
   private actor_id_: ActorId;
@@ -20,5 +37,7 @@ export class Proposer {
     
   }
 }
+
+
 
 }
